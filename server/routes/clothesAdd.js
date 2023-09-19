2dc171b4c5ce37efb1cf1453b7eeb09ad4015ff4
Ashutosh <rajputashu7200@gmail.com>
2023-09-19 21:45:13 +0530

    partial searching

--- server/routes/clothesAdd.js
+++ server/routes/clothesAdd.js
@@ -10,15 +10,15 @@ router.post('/clothesAdd',async(req,res)=>{
         return;
     }
     try {
-        const clothe=new clothes({
-            product:product,
-            size:size,
-            gender:gender,
-            colour:colour,
-            price:price,
-            brand:brand
-        })
-
+        const obj=req.body
+        console.log(obj)
+        // converting all data to lower case to remove the case of pattern matching
+        for(const key in obj){
+            if(typeof(obj[key])=== 'string'){
+                obj[key]=obj[key].toLowerCase();
+            }
+        }
+        const clothe=new clothes(obj);
         const result=await clothes.collection.insertOne(clothe)
         res.status(201).send('Clothe Added Successfully');
     } catch (error) {
--- server/routes/searchAndSort.js
+++ server/routes/searchAndSort.js
@@ -6,16 +6,77 @@ const clothes=require('../models/clothes')
 router.get('/searchandsort',async(req,res)=>{
     const {gender,product,brand,size,colour,sor}=req.body;
     try {
-        const result=await clothes.find({
-            $and:[
-                {gender:gender},
-                {product:product},
-                {brand:brand},
-                {size:size},
-                {colour:colour}
-            ]
-        }).sort({price:sor})
-        res.status(201).send(result); 
+        // search box
+            const search=req.query.search||""
+            // console.log(search)
+            const r=String(search)
+            const searchArray=r.split(' ');
+            console.log(searchArray)
+
+            // to convert each word to lowercase to fuck regex matching
+            const lowerCaseSearch=[];
+
+            for (const word of searchArray){
+                lowerCaseSearch.push(word.toLowerCase());
+            }
+
+            console.log(lowerCaseSearch)
+
+            const defaultProduct=await clothes.find()
+            const prodarr=[]
+            const brandarr=[]
+            const genderarr=[]
+            const colourarr=[]
+            
+            for (let i = 0; i < defaultProduct.length; i++) {
+                const productObject = defaultProduct[i];
+                const product = productObject.product;
+                const gender = productObject.gender;
+                const brand = productObject.brand;
+                const colour = productObject.colour;
+                prodarr.push(product)
+                brandarr.push(brand)
+                genderarr.push(gender)
+                colourarr.push(colour)
+                // console.log(`Product: ${product}, Size: ${size}, Brand: ${brand}, Price: ${price}`);
+              }
+            // const regexPattern=new RegExp('^' + search)
+            // const query = { brand: { $regex: regexPattern, $options: 'i' } };
+            const query={
+                $or: [
+                    { product: { $in: lowerCaseSearch } },
+                    { product: { $in: prodarr } },
+                  ],
+                $or: [
+                    { brand: { $in: lowerCaseSearch } },
+                    { brand: { $in: brandarr } },
+                  ]
+            }
+            const result=await clothes.find(query);
+            res.status(201).send(result);
+
+
+
+            // const defaultProduct=await clothes.find()
+            
+
+
+
+
+
+
+
+
+        // const result=await clothes.find({
+        //     $and:[
+        //         // {gender:gender},
+        //         {product:product},
+        //         {brand:brand},
+        //         {size:size}
+        //         // {colour:colour}
+        //     ]
+        // }).sort({price:sor})
+        // res.status(201).send(result); 
     } catch (error) {
         res.status(404).send(error);
     }
